7d405ec2e9d3a1041bf426caf612172605bb5567
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-11-09 12:18:02 +0700

    Add files via upload

---
+++ calc-ram.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## Nam-Nguyen
+## 24/10/2023
+# Initialize a total memory variable
+total_memory=0
+
+# Use ps to list all running processes, exclude the header, and calculate memory usage for each service in MB
+ps aux --sort -rss | awk 'NR > 1 {
+    service_name = $11;
+    memory_usage = $6 / 1024; # Convert memory usage from KB to MB
+    service_memory[service_name] += memory_usage;
+}
+END {
+    printf "%-30s %s\n", "Service", "Memory Usage (MB)";
+    for (service in service_memory) {
+        if (service_memory[service] > 0) {
+            printf "%-30s %.2f MB\n", service, service_memory[service];
+            total_memory += service_memory[service];
+        }
+    }
+    printf "%-30s %.2f MB\n", "Total Memory Usage:", total_memory;
+}' | sort -k2,2nr
