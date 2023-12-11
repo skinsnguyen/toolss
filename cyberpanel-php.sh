3454c6cb76577bbcdbf8dcf34337797650d62e51
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-12-12 00:16:57 +0700

    Update cyberpanel-php.sh

--- cyberpanel-php.sh
+++ cyberpanel-php.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 # Tăng các thống ở configurations all tất php
-configurations="
-max_execution_time=100
-max_input_time=600
-max_input_vars=5000
-memory_limit=1024M
-post_max_size=512M
-upload_max_filesize=1G
-"
+lsws_path="/usr/local/lsws"
+if [ -d "${lsws_path}" ]; then
+    configurations="
+    max_execution_time=100
+    max_input_time=600
+    max_input_vars=5000
+    memory_limit=1024M
+    post_max_size=512M
+    upload_max_filesize=1G
+    "
+for i in $(ls "${lsws_path}" | grep 'lsphp*'); do
+        php_d_path="${lsws_path}/${i}/etc/php.d"
+        nam_ini_file="${php_d_path}/namnh-az.ini"
 
-for i in $(ls /usr/local/lsws/ | grep 'lsphp*'); do
-    php_d_path="/usr/local/lsws/${i}/etc/php.d"
-    nam_ini_file="${php_d_path}/nam.ini"
+        echo "Adding configurations to ${nam_ini_file}"
 
-    echo "Adding configurations to ${nam_ini_file}"
+        echo "${configurations}" >> "${nam_ini_file}"
 
-    echo "${configurations}" >> "${nam_ini_file}"
+        echo "Configurations added to ${nam_ini_file}"
+    done
+else
+    echo "Thư mục ${lsws_path} không tồn tại."
+fi
 
-    echo "Configurations added to ${nam_ini_file}"
-done
