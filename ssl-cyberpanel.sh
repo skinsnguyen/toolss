89480983651be6596f1863fa2b4e007730a6f4e4
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-12-20 02:28:45 +0700

    Update ssl-cyberpanel.sh

--- ssl-cyberpanel.sh
+++ ssl-cyberpanel.sh
@@ -16,5 +16,5 @@ echo "! kich hoat ssl cyberpanel !"
 read -p "Nhap domain kich hoat ssl : " domain
 wget -O - https://get.acme.sh | sh
 /root/.acme.sh/acme.sh --register-account -m ${domain}@gmail.com
-/root/.acme.sh/acme.sh --issue -d ${domain} -d www.${domain} --cert-file /etc/letsencrypt/live/${domain}/cert.pem --key-file /etc/letsencrypt/live/${domain}/privkey.pem --fullchain-file /etc/letsencrypt/live/${domain}/fullchain.pem -w /home/nova4x4.vn/public_html --force
+/root/.acme.sh/acme.sh --issue -d ${domain} -d www.${domain} --cert-file /etc/letsencrypt/live/${domain}/cert.pem --key-file /etc/letsencrypt/live/${domain}/privkey.pem --fullchain-file /etc/letsencrypt/live/${domain}/fullchain.pem -w /home/${domain}/public_html --force
 rm -f ${pwdd}/ssl-cyberpanel.sh
