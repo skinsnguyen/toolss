703fc8dd0911d7baf4a4ecd16c2a6e2720e5a70e
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-12-27 09:19:16 +0700

    Update kill-mysql-loop.sh

--- kill-mysql-loop.sh
+++ kill-mysql-loop.sh
@@ -14,11 +14,11 @@ while true; do
     # Lấy ra các lệnh KILL cho các phiên kết nối từ user 'admin_home'
     mysql -e "Select concat('KILL ',id,';') from information_schema.processlist where user='$user_name'" | tail -n +2 | tr -d "|" | while read -r kill_command; do
         # Thực hiện lệnh KILL
-        echo "Tiến thành kill các ID của '$user_name' trong MySQL."
+
         mysql -e "$kill_command"
         echo "Executed: $kill_command"
     done
-    
+    echo "Tiến thành kill các ID của $user_name trong MySQL."
     # Chờ một khoảng thời gian trước khi lặp lại
     sleep 10
 done
